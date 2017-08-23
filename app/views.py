595a2d97a440d650340e437a59137649b78a3df4
Dennis Nderitu <dknderitu@gmail.com>
2017-08-23 14:59:40 +0300

    working on templates

--- app/views.py
+++ app/views.py
@@ -2,5 +2,22 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
+
 def index():
-	return "Hello, World!"
+	user = {'nickname':'Dennis'} # Real User
+	posts = [ #Fake array of posts
+	{
+		'author':{'nickname':'John'},
+		'body': 'Beautiful day in portland!'
+	},
+	{
+		'author':{'nickname':'Susan'},
+		'body':'The Avengers movie was so cool!'
+	}
+
+
+	]
+	return render_template('index.html',
+		                   title='Home',
+		                   user=user)
+
