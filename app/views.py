dc5bf2005c5a12d8f1baf90cc5fc49a42328aed2
Dennis Nderitu <dknderitu@gmail.com>
2017-08-23 15:15:46 +0300

    working on some web templates

--- app/views.py
+++ app/views.py
@@ -6,18 +6,20 @@ from app import app
 def index():
 	user = {'nickname':'Dennis'} # Real User
 	posts = [ #Fake array of posts
-	{
-		'author':{'nickname':'John'},
-		'body': 'Beautiful day in portland!'
-	},
-	{
-		'author':{'nickname':'Susan'},
-		'body':'The Avengers movie was so cool!'
-	}
+		{
+			'author':{'nickname':'John'},
+			'body': 'Beautiful day in portland!'
+		},
+		{
+			'author':{'nickname':'Susan'},
+			'body':'The Avengers movie was so cool!'
+		}
 
 
 	]
-	return render_template('index.html',
+
+	return render_template("index.html",
 		                   title='Home',
-		                   user=user)
+		                   user=user,
+		                   posts=posts)
 
