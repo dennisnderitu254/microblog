8380815edb8e08a5793729811e83d1354415bc0f
Dennis Nderitu <dknderitu@gmail.com>
2017-08-28 15:55:36 +0300

    coding db migrations upgrades and downgrades

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 
 
-class User(db.model):
+class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	nickname = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
