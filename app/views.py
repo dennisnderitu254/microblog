2ef1c5d2301e3a0a1d6d4bb6e90d497983878d95
Dennis Nderitu <dknderitu@gmail.com>
2017-08-29 16:55:30 +0300

    coding after login

--- app/views.py
+++ app/views.py
@@ -4,6 +4,17 @@ from app import app, db, lm, oid
 from .forms import LoginForm
 from .models import User
 
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
+
 @app.route('/')
 @app.route('/index')
 @login_required
@@ -43,6 +54,27 @@ def login():
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
 
 @app.route('/logout')
 def logout():
