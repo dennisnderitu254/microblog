d531fefd73674336dc47847341fac4dd92cc51f6
Dennis Nderitu <dknderitu@gmail.com>
2017-08-29 16:49:38 +0300

    reviewing changes

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,13 @@ from flask_login import LoginManager
 from flask_openid import OpenID
 from config import basedir
 
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view ='login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
 
 from app import views, models
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-#!/usr/bin/env python
+#!flask/bin/python
 from app import app
 app.run(debug=True)
