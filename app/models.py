6a94c58f5bd55cd65551dcb0c123193d1145bcaa
Dennis Nderitu <dknderitu@gmail.com>
2017-09-05 17:19:03 +0300

    adding avatar function

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from app import db
 
 
@@ -10,6 +11,18 @@ class User(db.Model):
 	last_seen = db.Column(db.DateTime)
 	def avatar(self,size):
 		return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' %(md5(self.email.encode('utf-8')).hexdigest(), size)
+
+	@staticmethod
+	def make_unique_nickname(nickname):
+		if User.query.filter_by(nickname=nickname).first() is None:
+			return nickname
+		version = 2
+		while True:
+			new_nickname = nickname + str(version)
+			if User.query.filter_by(nickname=new_nickname).first() is None:
+				break
+			version += 1
+		return new_nickname
 
 
 	@property
@@ -31,6 +44,11 @@ class User(db.Model):
 			return unicode(self.id) # python 2
 		except NameError:
 			return str(self.id)  # Python 3
+
+	def avatar(self, size):
+		return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % \
+		(md5(self.email.encode('utf-8')).hexdigest(), size)
+		
 
 	def __repr__(self):
 		return '<user %r>' % (self.nickname)
