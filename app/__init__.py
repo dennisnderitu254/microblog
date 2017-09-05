feb4d8952d60e3fa22956dc39692c21be0b0a242
Dennis Nderitu <dknderitu@gmail.com>
2017-09-05 17:23:23 +0300

    importing handlers in debugging

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_openid import OpenID
-from config import basedir
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, \
+	MAIL_PASSWORD
+
+
 
 
 app = Flask(__name__)
@@ -13,5 +16,27 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view ='login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+if not app.debug:
+	import logging
+	from logging.handlers import STMPHandler
+	credentials = None
+	if MAIL_USERNAME or MAIL_PASSWORD:
+		credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+	mail_handler = STMPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
+	mail_handler.setLevel(logging.ERROR)
+	app.logger.addHandler(mail_handler)
+
+if not app.debug:
+	import logging
+	from logging.handlers import RotatingFileHandler
+	file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1*1024*1024,10)
+
+	file_handler.setlevel(logging.INFO)
+	file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+	app.logger.addHandler(file_handler)
+	app.logger.setLevel(logging.INFO)
+	app.logger.info('microblog startup')
 
 from app import views, models
