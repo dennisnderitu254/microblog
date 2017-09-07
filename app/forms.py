7619cda9738075b39d7ad3e2e1e7fbed116759e3
Dennis Nderitu <dknderitu@gmail.com>
2017-09-07 17:36:30 +0300

    validating nickname in the login form

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,19 @@
 from flask_wtf import Form
 from wtforms import StringField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length
+from .models import User
+
 
 class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
 
-# Editing profile information
-class EditForm(Form):
-	nickname = StringField('nickname', validators=[DataRequired()])
-	about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
 
-	def __init__(self, original_nickname, *args, **kwargs):
+class EditForm(Form):
+    nickname = StringField('nickname', validators=[DataRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
+
+    def __init__(self, original_nickname, *args, **kwargs):
         Form.__init__(self, *args, **kwargs)
         self.original_nickname = original_nickname
 
