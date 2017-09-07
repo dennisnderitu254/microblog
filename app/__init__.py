50ae0bf3d138535444ab51ceaa4cd8d1b6eb7a9f
Dennis Nderitu <dknderitu@gmail.com>
2017-09-07 17:47:38 +0300

    importing logging handlers

--- app/__init__.py
+++ app/__init__.py
@@ -7,36 +7,36 @@ from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, \
 	MAIL_PASSWORD
 
 
-
-
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
-lm.login_view ='login'
+lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 if not app.debug:
-	import logging
-	from logging.handlers import STMPHandler
-	credentials = None
-	if MAIL_USERNAME or MAIL_PASSWORD:
-		credentials = (MAIL_USERNAME, MAIL_PASSWORD)
-	mail_handler = STMPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
-	mail_handler.setLevel(logging.ERROR)
-	app.logger.addHandler(mail_handler)
+    import logging
+    from logging.handlers import SMTPHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT),
+                               'no-reply@' + MAIL_SERVER, ADMINS,
+                               'microblog failure', credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
 
 if not app.debug:
-	import logging
-	from logging.handlers import RotatingFileHandler
-	file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1*1024*1024,10)
-
-	file_handler.setlevel(logging.INFO)
-	file_handler.setFormatter(logging.Formatter(
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a',
+                                       1 * 1024 * 1024, 10)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-	app.logger.addHandler(file_handler)
-	app.logger.setLevel(logging.INFO)
-	app.logger.info('microblog startup')
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
 
 from app import views, models
