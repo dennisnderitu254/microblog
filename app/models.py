cb5c332b7857c1b1b2f1865c9ded0db2204f35e6
Dennis Nderitu <dknderitu@gmail.com>
2017-09-11 11:25:13 +0300

    rechecking  followers db table

--- app/models.py
+++ app/models.py
@@ -3,10 +3,8 @@ from app import db
 
 followers = db.Table('followers',
 	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-	db.column('followed_id', db.Integer, db.ForeignKey(user.id))
+	db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
-
-
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -23,23 +21,8 @@ class User(db.Model):
 								secondaryjoin=(followers.c.followed_id==id),
 								backref=db.backref('followers', lazy='dynamic'),
 								lazy='dynamic')
-	def follow(self, user):
-		if not self.is_following(user):
-			self.followed.append(user)
-			return self
 
-	def unfollow(self, user):
-		if self.is_following(user):
-			self.followed.remove(user)
-			return self
-
-	def is_following(self, user):
-		return self.followed.filter(followers.c.followed_id == user.id).count() > 0
-
-	def followed_posts(self):
-		return Post.query.join(followers,(followers.c.followed_id == Post.user_id)).filter(followers.c.followed_id == self.id).order_by(Post.timestamp.desc())
-
-
+	
 	@staticmethod
 	def make_unique_nickname(nickname):
 		if User.query.filter_by(nickname=nickname).first() is None:
@@ -76,6 +59,24 @@ class User(db.Model):
 	def avatar(self, size):
 		return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % \
 		(md5(self.email.encode('utf-8')).hexdigest(), size)
+
+	def follow(self, user):
+		if not self.is_following(user):
+			self.followed.append(user)
+			return self
+
+	def unfollow(self, user):
+		if self.is_following(user):
+			self.followed.remove(user)
+			return self
+
+	def is_following(self, user):
+		return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+	def followed_posts(self):
+		return Post.query.join(followers,(followers.c.followed_id == Post.user_id)).filter(followers.c.followed_id == self.id).order_by(Post.timestamp.desc())
+
+
 		
 
 	def __repr__(self):
