a8d40a7714e738e6574559a4b7f58862480595d5
Dennis Nderitu <dknderitu@gmail.com>
2017-09-14 19:35:31 +0300

    adding page navigation feature

--- app/views.py
+++ app/views.py
@@ -91,20 +91,18 @@ def after_login(resp):
 
 
 @app.route('/user/<nickname>')
+@app.route('/user/<nickname>/<int:page>')
 @login_required
-def user(nickname):
+def user(nickname, page=1):
     user = User.query.filter_by(nickname=nickname).first()
     if user == None:
         flash('User %s not found.' % nickname)
         return redirect(url_for('index'))
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-
-    ]
+    posts = user.posts.paginate(page, POSTS_PER_PAGE, False)
     return render_template('user.html',
-                                                user=user,
-                                                posts=posts)
+                           user=user,
+                           posts=posts)
+                                                   
 
 @app.route('/logout')
 def logout():
