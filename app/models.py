9e7daa627afb00b03ef8127e3c81a1dade5d1d88
Dennis Nderitu <dknderitu@gmail.com>
2017-09-17 20:18:35 +0300

    enabling search

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from hashlib import md5
 from app import db
+from app import app
 
 followers = db.Table('followers',
 	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -90,6 +91,30 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
+
+
+import sys
+if sys.version_info >= (3, 0):
+	enable_search = False
+
+else:
+	enable_search = True
+	import flask_whooshalchemy as whooshalchemy
+
+class Post(db.Model):
+	__searchable__ = ['body']
+
+	id = db.Column(db.Integer, primary_key=True)
+	body = db.Column(db.String(140))
+	timestamp = db.Column(db.DateTime)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+	def __repr__(self):
+		return '<Post %r>' % (self.body)
+
+if enable_search:
+	whooshalchemy.whoosh_index(app, Post)
 
 
 
