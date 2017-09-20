f98f476eb367b7069c6e8d5eaa76b346d2856d39
Dennis Nderitu <dknderitu@gmail.com>
2017-09-20 20:50:58 +0300

    Implimenting asynchronous emails

--- app/emails.py
+++ app/emails.py
@@ -2,6 +2,16 @@ from flask_mail import Message
 from app import flask_mail
 from flask import render_template
 from config import ADMINS
+from thread import thread
+from threading import Thread
+from app import app
+
+
+@async
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
 
 def send_email(subject, sender, recipients, textbody, htmlbody):
 	msg = Message(subject, sender=sender, recipients=recipients)
@@ -17,3 +27,10 @@ def follower_notification(followed, follower):
 		       			       user=followed, follower=follower),
 		       render_template("follower_email.html",
 		       	 			   user=followed, follower=follower))
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    send_async_email(app, msg)
+
